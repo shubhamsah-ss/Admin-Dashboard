c8e9f6e12acd0d2d92d8c11c0bcde6ed42d47a22
Shubham Kumar Sah <shubham.6552@gmail.com>
2023-12-09 16:07:30 +0530

    complete

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Login from './login/page'
+import Link from 'next/link'
 
 const Home = () => {
+
   return (
     <div>
-        <Login />
+        <Link href={"/login"}>Login</Link>
+        <Link href={"/dashboard"}>Dashboard</Link>
     </div>
   )
 }
